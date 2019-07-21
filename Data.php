d07f8956e601c158f55c224a875fba04a327c361
kuriapaul <pauwains@gmail.com>
2019-07-21 10:50:09 +0300

    initial commit

---
+++ Data.php
@@ -0,0 +1,8 @@
+<?php 
+$DatabaseType = 'mysqli'; 
+$DatabaseServer = 'localhost'; 
+$DatabaseUsername = 'root'; 
+$DatabasePassword = 'admin@2019'; 
+$DatabaseName = 'opensis'; 
+$DatabasePort = '3306'; 
+?>
